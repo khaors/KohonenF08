4f16ed2a20491375c3384291bcb9e0284ae59a2e
khaors <khaors@gmail.com>
2025-03-16 15:37:14 -0500

    mt19937 random number generator included in test_random

--- test/test_random.f90
+++ test/test_random.f90
@@ -1,16 +1,21 @@
 module test_random
+    use iso_fortran_env, only: output_unit, real64, int64
     use testdrive, only: new_unittest, unittest_type, error_type,check
     use precision_utilities, only: wp;
     use rkiss05_generator_utilities, only: rkiss05_generator;
+    use mt19937_64, only: mt19937;
     implicit none;
     private;
     type(rkiss05_generator) :: my_grator
+    type(mt19937) :: my_mt_grator
     integer :: seed
+    integer(int64) :: seed1
     public :: collect_random
 contains
     subroutine collect_random(testsuite)
         type(unittest_type),allocatable,intent(out) :: testsuite(:)
-        testsuite=[new_unittest("RKISS", test_rkiss)]
+        testsuite=[new_unittest("RKISS", test_rkiss),&
+                   new_unittest('mt19937', test_mt19937)]
     end subroutine collect_random
     !
     subroutine test_rkiss(error)
@@ -36,6 +41,29 @@ contains
         call my_grator%destroy();
     end subroutine test_rkiss
 !
+    subroutine test_mt19937(error)
+        type(error_type),allocatable,intent(out) :: error
+        !
+        integer :: i
+        real(kind=wp),allocatable :: rnumbers(:),rnumbers1(:)
+        !
+        rnumbers=(/(dble(i),i=1,5)/);
+        rnumbers1=(/(dble(i),i=1,5)/); 
+        seed1=12345;
+        call my_mt_grator%initialize(seed1);
+        do i=1,5;
+            rnumbers(i)=my_mt_grator%genrand64_real1();
+        end do
+        call my_mt_grator%initialize(seed1);
+        do i=1,5;
+            rnumbers1(i)=my_mt_grator%genrand64_real1();
+        end do
+        do i=1,5;
+            call check(error,rnumbers(i),rnumbers1(i));
+        enddo
+        if(allocated(error)) return;
+        
+    end subroutine test_mt19937
 end module test_random
 
 program tester
