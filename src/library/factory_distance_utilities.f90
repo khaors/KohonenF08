06d4d4ca70a64a82c4a6a3f7d358e4f6e0f8c2f2
khaors <khaors@gmail.com>
2025-03-16 21:29:38 -0500

    correlation and direction cosine distances added to the factory

--- src/library/factory_distance_utilities.f90
+++ src/library/factory_distance_utilities.f90
@@ -4,10 +4,14 @@
 !! This module defines a factory to create distance objects 
 module factory_distance_utilities
 !! This module defines a factory to create distance objects 
+use error_handling, only: error_stop;
+use constants_utilities, only: NUMCHAR;
 use distance_base_utilities, only: distance_base;
 use euclidean_distance_utilities, only: euclidean_distance;
 use manhattan_distance_utilities, only: manhattan_distance;
 use max_distance_utilities, only: max_distance;
+use correlation_distance_utilities, only: correlation_distance;
+use direction_cosine_distance_utilities, only: direction_cosine_distance;
 !
 implicit none;
 !
@@ -28,18 +32,42 @@ contains
 !! A character string with the type of distance to be instantiated
         class(distance_base),allocatable :: dist
 !! An allocatable `distance_base` object
+        integer :: ierr
+        character(len=NUMCHAR) :: base_message,message
+!
+        base_message='FACTORY DISTANCE ERROR';
+        if(allocated(dist)) deallocate(dist);
         select case(trim(type_))
           case('euclidean')
-            !write(*,*) 'Euclidean distance';
-            if(allocated(dist)) deallocate(dist);
-            allocate(euclidean_distance :: dist);
+            allocate(euclidean_distance :: dist,stat=ierr);
+            if(ierr /= 0) then
+                message=trim(base_message)//' while allocating memory for euclidean distance';
+                call error_stop(message);
+            end if
           case('manhattan')
-    !         !write(*,*) 'Manhattan distance allocated';
-            if(allocated(dist)) deallocate(dist);
-            allocate(manhattan_distance :: dist);
+            allocate(manhattan_distance :: dist,stat=ierr);
+            if(ierr /= 0) then
+                message=trim(base_message)//' while allocating memory for manhattan distance';
+                call error_stop(message);
+            end if
         case('max')
-            if(allocated(dist)) deallocate(dist);
-            allocate(max_distance :: dist);
+            allocate(max_distance :: dist,stat=ierr);
+            if(ierr /= 0) then
+                message=trim(base_message)//' while allocating memory for max distance';
+                call error_stop(message);
+            end if
+        case('correlation')
+            allocate(correlation_distance :: dist,stat=ierr);
+            if(ierr /= 0) then
+                message=trim(base_message)//' while allocating memory for correlation distance';
+                call error_stop(message);
+            end if
+        case('direction_cosine')
+            allocate(direction_cosine_distance :: dist,stat=ierr);
+            if(ierr /= 0) then
+                message=trim(base_message)//' while allocating memory for direction cosine distance';
+                call error_stop(message);
+            end if
           case default
             write(*,*) 'ERROR: the requested distance is not defined'
             stop
