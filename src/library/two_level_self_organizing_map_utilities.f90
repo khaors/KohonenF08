12ed2563ea741a06e0611595320fe867a9ad5284
khaors <khaors@gmail.com>
2025-11-29 11:45:01 -0500

    Headers added to çtwo_level_self_organizing_map_uttilities

--- src/library/two_level_self_organizing_map_utilities.f90
+++ src/library/two_level_self_organizing_map_utilities.f90
@@ -4,15 +4,18 @@
 !! This module defines a class that represents a two layer self_organizing_map  for clustering 
 module two_level_self_organizing_map_utilities
 !! This module defines a class that represents a two layer self_organizing_map  for clustering
+   use error_handling, only: error_t,error_stop;
    use precision_utilities, only: wp;
-   use kohonen_layer_parameters_utilities;
-   use kohonen_map_base_utilities;
-   use kohonen_prototype_utilities;
-   use kohonen_pattern_utilities;
-   use distance_base_utilities;
-   use factory_distance_utilities;
+   use constants_utilities, only: NUMCHAR;
    use random_generator_base_utilities, only: random_generator_base;
    use rkiss05_generator_utilities, only: rkiss05_generator;
+   use kohonen_layer_parameters_utilities, only: kohonen_layer_parameters;
+   use kohonen_map_base_utilities, only: kohonen_map_base;
+   use kohonen_prototype_utilities, only: kohonen_prototype;
+   use kohonen_pattern_utilities, only: kohonen_pattern;
+   use distance_base_utilities, only: distance_base;
+   use factory_distance_utilities, only: factory_distance;   
+
    !
    implicit none;
    !
